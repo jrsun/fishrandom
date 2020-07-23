3487d54c66be243b7c959387fdfc72dd3a40c7c4
Jeffrey Sun <jeffreyrnsun@gmail.com>
2020-07-22 21:57:53 -0700

    Webpack reads js produced by tsc

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/ui/my-element.ts',
+  entry: './dist/ui/my-element.js',
   devtool: 'inline-source-map',
+  mode: 'development',
   module: {
     rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+      // {
+      //   test: /\.tsx?$/,
+      //   use: 'ts-loader',
+      //   exclude: /node_modules/,
+      // },
     ],
   },
   resolve: {
