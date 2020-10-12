be9c73f6aec7c4b6b313e5189cdc5d19fe432a62
Jeffrey Sun <jeffreyrnsun@gmail.com>
2020-10-12 17:33:41 -0400

    remove comments

--- src/server/server.ts
+++ src/server/server.ts
@@ -262,27 +262,6 @@ const handleMessage = async function (
   }
   room.handleMessage(player.uuid, message);
 
-  // if (message.type === 'turn') {
-  //   // sanitize
-  //   function tg(message: Message): message is TurnMessage {
-  //     return message.type === 'turn';
-  //   }
-  //   if (!tg(message)) {
-  //     log.warn('message is not valid turn message', message);
-  //     return;
-  //   }
-  //   try {
-  //     room.handleTurn(player.uuid, message.turn);
-  //   } catch (e) {
-  //     log.error('ERR: fatal turn error', e);
-  //   }
-  // }
-  // if (message.type === 'roomAction') {
-  //   room.handleAction(player.uuid, message.action);
-  // }
-  // if (message.type === 'getAllowed') {
-  //   room.handleGetAllowed(player.uuid);
-  // }
   if (room.phase === PhaseEnum.DONE) {
     playerLog.notice('game completed');
   }
